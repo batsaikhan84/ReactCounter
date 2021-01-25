8dd598a7286206f7548ebb2163e9af511492070e
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-01-24 21:19:32 -0600

    folder change

---
+++ v1/src/components/Counter.js
@@ -0,0 +1,51 @@
+import { Component } from "react"
+
+class Counter extends Component {
+    constructor() {
+        super()
+        this.state = {
+          count: 0
+        }
+      }
+      increment = () => {
+        this.setState({
+          count: this.state.count + 1
+        })
+      }
+      decrement = () => {
+        this.setState({
+          count: this.state.count - 1
+        })
+      }
+      reset = () => {
+        this.setState({
+          count: 0
+        })
+      }
+      render() {
+        return (
+          <div className = "counterContainer">
+            { this.state.count }
+            <button className = "decrement" onClick = { this.decrement }>-</button>
+            <button className = "reset" onClick = { this.reset }>Reset</button>
+            <button className = "increment" onClick = { this.increment }>+</button>
+            <button className = "countStart" onClick = { this.counterStart }>Start</button>
+            <button className = "countStop" onClick = { this.couonterStop }>Stop</button>
+            
+          </div>
+        )
+      }
+      counterStart = () => {
+        this.myInterval = setInterval(() => {
+          this.setState({
+            count: this.state.count + 1
+          })
+        }, 1000);
+      }
+      couonterStop = () => {
+        clearInterval(this.myInterval)
+      }
+
+}
+
+export default Counter
